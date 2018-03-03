53179559212076419cbd27605fdb7ddde1019634
Mike Falanga <m.falanga@gmail.com>
2018-03-03 16:56:25 -0500

    Add structure to color

--- trap.ino
+++ trap.ino
@@ -4,46 +4,56 @@
 
 #define COMMON_ANODE
 
-int redPin = 9;
-int greenPin = 10;
-int bluePin = 11;
+struct Color {
+  int red;
+  int green;
+  int blue;
+};
 
 int led[3] = {9, 10, 11};
 
-void setup() {
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-}
+Color RED    = { 255, 0, 0 };
+Color ORANGE = { 255, 127, 0 };
+Color YELLOW = { 255, 255, 0 };
+Color GREEN  = { 0, 255, 0 };
+Color BLUE   = { 0, 0, 255 };
+Color VIOLET = { 148, 0, 211 };
 
-void loop() {
-    rainbow();
-}
+Color rainbow[6] = { RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET };
 
-void rainbow() {
-  setColor(led, 255, 0, 0);  // red
-  delay(500);
-  setColor(led, 255, 127, 0); // orange (#FF7F00)
-  delay(500);
-  setColor(led, 255, 255, 0);  // yellow
-  delay(500);
-  setColor(led, 0, 255, 0);  // green
-  delay(500);
-  setColor(led, 0, 0, 255);  // blue
-  delay(500);
-  setColor(led, 148, 0, 211);  // violet
-  delay(500);
-}
+void setColor(int led[], Color color) {
+  int red = color.red;
+  int green = color.green;
+  int blue = color.blue;
 
-void setColor(int pin[], int red, int green, int blue)
-{
   #ifdef COMMON_ANODE
     red = 255 - red;
     green = 255 - green;
     blue = 255 - blue;
   #endif
 
-  analogWrite(pin[0], red);
-  analogWrite(pin[1], green);
-  analogWrite(pin[2], blue);
+  analogWrite(led[0], red);
+  analogWrite(led[1], green);
+  analogWrite(led[2], blue);
+}
+
+void cycleColors(int led[], Color colors[], int duration) {
+  for(int i=0; i<6; i++) {
+    setColor(led, colors[i]);
+    delay(duration);
+  }
+}
+
+void setupLed(int led[]) {
+  pinMode(led[0], OUTPUT);
+  pinMode(led[1], OUTPUT);
+  pinMode(led[2], OUTPUT);
+}
+
+void setup() {
+  setupLed(led);
+}
+
+void loop() {
+  cycleColors(led, rainbow, 500);
 }
